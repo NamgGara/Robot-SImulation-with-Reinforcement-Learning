2503da8f4bcbf25138d361c76d5532ebd72335c8
NamgGara <garab.python@gmail.com>
2022-08-01 19:03:50 +0600

    test on articulations

--- simulation.py
+++ simulation.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import pybullet_data
+import random
 from time import sleep
+import model
 
 physics_client = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -11,10 +13,17 @@ plane = p.loadURDF("plane.urdf")
 spawn_point = [0,0,3]
 spawn_pitch = p.getQuaternionFromEuler([0,0,0])
 robot = p.loadURDF("humanoid.urdf", spawn_point, spawn_pitch)
+joint_array = range(p.getNumJoints(robot))
 
 if __name__== "__main__":
     for i in range(10000):
         p.stepSimulation()
+        #motor control test
+        articulation = [random.random()*100 for i in joint_array]
+        p.setJointMotorControlArray(robot, joint_array, p.POSITION_CONTROL, articulation,[1.0 for i in joint_array])
+
+        print(p.getJointStates(robot))
+        
         sleep(1./240.)
 
     p.disconnect()
