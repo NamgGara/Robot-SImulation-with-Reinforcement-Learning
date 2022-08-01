91b676f1ed451f03153ef967968eab7af4e0f6da
NamgGara <garab.python@gmail.com>
2022-08-01 19:15:39 +0600

    model.py module that would hold the model classes

---
+++ model.py
@@ -0,0 +1,23 @@
+import torch 
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torch.optim
+import torch.utils.data
+
+class RL:
+    def __init__(self):
+        pass
+
+    def forward(self,joint_list):
+
+        #logic
+
+        return 
+
+class NN:
+    def __init__(self) -> None:
+        pass
+
+rl_model = RL()
+
+from simulation import joint_states
