445892dce9eddcb97cff40da86e9cc31e453acff
NamgGara <garab.python@gmail.com>
2022-08-02 19:30:09 +0600

    moved tunable parameters together

--- simulation.py
+++ simulation.py
@@ -2,21 +2,22 @@ import pybullet as p
 import pybullet_data
 import random
 from time import sleep
-
 import torch
 import model
 from torch import save as _save
 
-physics_client = p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0,0,-9.8)
-
-plane = p.loadURDF("plane.urdf")
-
+hyper_parameters = model.Model_HyperParameters()
+robot_head = 2
+gravity = [0,0,-9.9]
 spawn_point = [0,0,3]
 spawn_pitch = p.getQuaternionFromEuler([0,0,0])
-robot = p.loadURDF("humanoid.urdf", spawn_point, spawn_pitch)
+model = "humanoid.urdf"
 
+physics_client = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(*gravity)
+plane = p.loadURDF("plane.urdf")
+robot = p.loadURDF(model, spawn_point, spawn_pitch)
 joint_array = range(p.getNumJoints(robot))
 feature_length = len(joint_array)
 
@@ -25,7 +26,7 @@ rl_model =model.ActorC()
 DQN_old = model.DQN(feature_length=feature_length)
 DQN_new = model.DQN(feature_length=feature_length)
 _save(DQN_new.state_dict(),"old_parameters")
-hyper_parameters = model.Model_HyperParameters()
+
 
 if __name__ == "__main__":
     for i in range(10000):
@@ -41,8 +42,6 @@ if __name__ == "__main__":
             old_target = DQN_old(states_as_tensors)
 
         #motor control test 
-
-
         sleep(1./240.)
 
         # i need loss function   reward + next state value fn - current state value fn
