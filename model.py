eefe0164f2ab488fcf9dfa560fc0a1d525302a89
NamgGara <garab.python@gmail.com>
2022-08-02 16:15:27 +0600

    reward_lock that prevents agent from abusing the env

--- model.py
+++ model.py
@@ -6,9 +6,26 @@ import torch.optim
 import torch.utils.data
 from simulation import robot, joint_array
 
-joint_states= getJointStates(robot, joint_array)
+""" 
+the model should be actor critic
 
-class RL:
+to do:
+what distribution to use for policy and how to select actions
+recieves states of all joints and produces position for each joint
+
+critic should be DQN
+the network should produce value function of states
+"""
+
+joint_states= getJointStates(robot, joint_array)
+feature_length = len(joint_array)
+
+
+def reward_lock(progress,threshold):
+    if progress > threshold:
+        return 1, (new_threshold:=progress)
+
+class ActorC:
     def __init__(self):
         pass
 
@@ -18,11 +35,14 @@ class RL:
 
         return 
 
-class NN:
-    def __init__(self) -> None:
-        pass
+class DQN:
+    def __init__(self):
+        self.dense_1 = nn.Linear(in_features= feature_length, out_features= feature_length + 4)
+        self.dense_2 = nn.Linear(in_features=feature_length + 4, out_features=feature_length-6)
+        self.final_dense = nn.Linear(in_features=feature_length-6, out_features=1)
+        
+rl_model = ActorC()
 
-rl_model = RL()
 for i in range(10):
     print(joint_states)
 
