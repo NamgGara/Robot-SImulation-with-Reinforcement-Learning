0f01e672e4a910e604fe1dd24ab96927bb2c670b
NamgGara <garab.python@gmail.com>
2022-08-02 18:35:22 +0600

    added reward logic, corrected super().__init__() for nn.Module

--- model.py
+++ model.py
@@ -6,11 +6,18 @@ import torch.optim
 import torch.utils.data
 from simulation import robot, joint_array
 
+discount = 0.9
+t_reward = -1
+c_reward = 0
+
+# i need loss function   reward + next state value fn - current state value fn
+
 joint_states= getJointStates(robot, joint_array)
 feature_length = len(joint_array)
 
-class DQN:
+class DQN(nn.Module):
     def __init__(self):
+        super().__init__()
         self.dense_1 = nn.Linear(in_features= feature_length, out_features= feature_length + 4)
         self.dense_2 = nn.Linear(in_features=feature_length + 4, out_features=feature_length-6)
         self.final_dense = nn.Linear(in_features=feature_length-6, out_features=1)
@@ -19,20 +26,22 @@ class DQN:
         result = self.dense_1(states)
         result = self.dense_2(result)
         return self.final_dense(result)
+    
 
-class ActorC:
+class ActorC(nn.Module):
     def __init__(self):
-        pass
+        super().__init__()
+        
 
     """ 
-the model should be actor critic
+    the model should be actor critic
 
-to do:
-what distribution to use for policy and how to select actions
-recieves states of all joints and produces position for each joint
+    to do:
+    what distribution to use for policy and how to select actions
+    recieves states of all joints and produces position for each joint
 
-critic should be DQN
-the network should produce value function of states
+    critic should be DQN
+    the network should produce value function of states
     """
 
     def forward(self,joint_list):
@@ -41,15 +50,23 @@ the network should produce value function of states
 
         return 
 
-
-
-
-def reward_lock(progress,threshold):
+def reward(progress,threshold):
     if progress > threshold:
-        return 1, (new_threshold:=progress)
+        return 10, (new_threshold:=progress)
 
 rl_model = ActorC()
+DQN_old = DQN()
+DQN_new = DQN()
+
+torch.save(DQN_old.state_dict(),"old_parameters")
 
 for i in range(10):
-    print(joint_states)
+
+    # print(joint_states)
+    # DQN_old = torch.loadst
+
+
+
+
+    
 
