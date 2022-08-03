ecb5a25462a2cd781eedc14577863eb3f863151f
NamgGara <garab.python@gmail.com>
2022-08-03 19:50:34 +0600

    moved code from model.py to simulation.py to avoid NaN error

--- simulation.py
+++ simulation.py
@@ -6,7 +6,57 @@ from time import sleep
 import torch
 import model
 
-hyper_parameters = model.Model_HyperParameters()
+import torch 
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+import torch.optim
+import torch.utils.data
+
+class Model_HyperParameters:
+    discount = 0.9
+    t_reward = -1
+    c_reward = 0
+
+class DQN(nn.Module):
+    def __init__(self,feature_length):
+        super().__init__()
+        self.dense_1 = nn.Linear(in_features= feature_length, out_features= feature_length + 4)
+        self.dense_2 = nn.Linear(in_features=feature_length + 4, out_features=feature_length-6)
+        self.final_dense = nn.Linear(in_features=feature_length-6, out_features=1)
+        
+    def forward(self,states):
+        result = F.relu(self.dense_1(states))
+        result = F.relu(self.dense_2(result))
+        # print(self.final_dense(result))
+        return self.final_dense(result)
+    
+
+class ActorC(nn.Module):
+    def __init__(self, feature_length):
+        super().__init__()
+        self.dense1 = nn.Linear(in_features=feature_length, out_features=40)
+        self.dense2 = nn.Linear(in_features=40, out_features=30)
+        self.final_mean = nn.Linear(in_features=30, out_features=feature_length)
+        self.final_std = nn.Linear(in_features=30, out_features=feature_length)
+
+    def forward(self,joint_list):
+
+        result = F.relu((self.dense1(joint_list)))
+        result = F.relu(self.dense2(result))
+        mean = self.final_mean(result)
+        std = self.final_std(result)
+        action = torch.distributions.Normal(loc=mean, scale=std)
+        return action.sample()
+
+def reward(progress,threshold):
+    if progress > threshold:
+        return 10, (new_threshold:=progress)
+    else:
+        return 0, threshold
+        
+
+hyper_parameters = Model_HyperParameters()
 robot_head = 2
 gravity = [0,0,-9.9]
 spawn_point = [0,0,3]
@@ -23,10 +73,9 @@ robot = p.loadURDF(urdf_model, spawn_point, spawn_pitch)
 joint_array = range(p.getNumJoints(robot))
 feature_length = len(joint_array)
 
-# from model.py
-rl_model = model.ActorC(feature_length=feature_length)
-DQN_old = model.DQN(feature_length=feature_length)
-DQN_new = model.DQN(feature_length=feature_length)
+rl_model = ActorC(feature_length=feature_length)
+DQN_old = DQN(feature_length=feature_length)
+DQN_new = DQN(feature_length=feature_length)
 
 if os.path.exists(save_path):
     for i in [DQN_old, DQN_new]:
@@ -71,5 +120,4 @@ if __name__ == "__main__":
         old_states_as_tensors = new_states_as_tensors
 
         sleep(1./150.)
-
     p.disconnect()
