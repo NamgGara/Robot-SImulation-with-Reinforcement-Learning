92d603ae79b3d0ee44498e494f2ff5a4082cc160
NamgGara <garab.python@gmail.com>
2022-08-03 20:40:01 +0600

    added model.py back

---
+++ model.py
@@ -0,0 +1,48 @@
+import torch 
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torch.optim
+import torch.utils.data
+
+class Model_HyperParameters:
+    discount = 0.9
+    t_reward = -1
+    c_reward = 0
+
+class DQN(nn.Module):
+    def __init__(self,feature_length):
+        super().__init__()
+        self.dense_1 = nn.Linear(in_features= feature_length, out_features= feature_length + 4)
+        self.dense_2 = nn.Linear(in_features=feature_length + 4, out_features=feature_length-6)
+        self.final_dense = nn.Linear(in_features=feature_length-6, out_features=1)
+        
+    def forward(self,states):
+        result = nn.ReLU()(self.dense_1(states))
+        result = nn.ReLU()(self.dense_2(result))
+        return self.final_dense(result)
+    
+
+class ActorC(nn.Module):
+    def __init__(self, feature_length):
+        super().__init__()
+        self.dense1 = nn.Linear(in_features=feature_length, out_features=40)
+        self.dense2 = nn.Linear(in_features=40, out_features=30)
+        self.final_mean = nn.Linear(in_features=30, out_features=feature_length)
+        self.final_std = nn.Linear(in_features=30, out_features=feature_length)
+
+    def forward(self,joint_list):
+
+        result = nn.Relu()(self.dense1(joint_list))
+        result = nn.ReLU()(self.dense2(result))
+        mean = self.final_mean(result)
+        std = self.final_std(result)
+        action = torch.distributions.Normal(loc=mean, scale=std)
+        return action.sample()
+
+def reward(progress,threshold):
+    if progress > threshold:
+        return 10, (new_threshold:=progress)
+    else:
+        return 0, threshold
+
+
--- simulation.py
+++ simulation.py
@@ -6,57 +6,7 @@ from time import sleep
 import torch
 import model
 
-import torch 
-import torch.nn as nn
-import torchvision.transforms as transforms
-import torch.nn.functional as F
-import torch.optim
-import torch.utils.data
-
-class Model_HyperParameters:
-    discount = 0.9
-    t_reward = -1
-    c_reward = 0
-
-class DQN(nn.Module):
-    def __init__(self,feature_length):
-        super().__init__()
-        self.dense_1 = nn.Linear(in_features= feature_length, out_features= feature_length + 4)
-        self.dense_2 = nn.Linear(in_features=feature_length + 4, out_features=feature_length-6)
-        self.final_dense = nn.Linear(in_features=feature_length-6, out_features=1)
-        
-    def forward(self,states):
-        result = F.relu(self.dense_1(states))
-        result = F.relu(self.dense_2(result))
-        # print(self.final_dense(result))
-        return self.final_dense(result)
-    
-
-class ActorC(nn.Module):
-    def __init__(self, feature_length):
-        super().__init__()
-        self.dense1 = nn.Linear(in_features=feature_length, out_features=40)
-        self.dense2 = nn.Linear(in_features=40, out_features=30)
-        self.final_mean = nn.Linear(in_features=30, out_features=feature_length)
-        self.final_std = nn.Linear(in_features=30, out_features=feature_length)
-
-    def forward(self,joint_list):
-
-        result = F.relu((self.dense1(joint_list)))
-        result = F.relu(self.dense2(result))
-        mean = self.final_mean(result)
-        std = self.final_std(result)
-        action = torch.distributions.Normal(loc=mean, scale=std)
-        return action.sample()
-
-def reward(progress,threshold):
-    if progress > threshold:
-        return 10, (new_threshold:=progress)
-    else:
-        return 0, threshold
-        
-
-hyper_parameters = Model_HyperParameters()
+hyper_parameters = model.Model_HyperParameters()
 robot_head = 2
 gravity = [0,0,-9.9]
 spawn_point = [0,0,3]
@@ -73,9 +23,10 @@ robot = p.loadURDF(urdf_model, spawn_point, spawn_pitch)
 joint_array = range(p.getNumJoints(robot))
 feature_length = len(joint_array)
 
-rl_model = ActorC(feature_length=feature_length)
-DQN_old = DQN(feature_length=feature_length)
-DQN_new = DQN(feature_length=feature_length)
+# from model.py
+rl_model = model.ActorC(feature_length=feature_length)
+DQN_old = model.DQN(feature_length=feature_length)
+DQN_new = model.DQN(feature_length=feature_length)
 
 if os.path.exists(save_path):
     for i in [DQN_old, DQN_new]:
@@ -91,6 +42,7 @@ optimizer = torch.optim.SGD(DQN_new.parameters(),lr=learning_rate_DQN)
 if __name__ == "__main__":
     for i in range(10000):
         optimizer.zero_grad()
+
         if i%100 == 0:
             DQN_old.load_state_dict(DQN_new.state_dict())
             torch.save(DQN_new.state_dict(),save_path)
@@ -109,15 +61,17 @@ if __name__ == "__main__":
 
         with torch.no_grad():
             old_target = DQN_old(new_states_as_tensors)
+        
         reward, threshold_cord = model.reward(new_head_coord, threshold_cord)
         hyper_parameters.c_reward += reward + hyper_parameters.t_reward
 
         delta = torch.nn.MSELoss()(torch.tensor(reward) + (hyper_parameters.discount * old_target), DQN_new(old_states_as_tensors))
         delta.backward()
         #ERROR 
-        # print(DQN_new.dense_1.weight.grad)
+        print(DQN_new.dense_1.weight.grad)
         optimizer.step()
         old_states_as_tensors = new_states_as_tensors
 
         sleep(1./150.)
+
     p.disconnect()
