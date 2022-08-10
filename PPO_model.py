126d3c12692ca1dbf651ceb4a098bb525f13a277
NamgGara <garab.python@gmail.com>
2022-08-10 12:58:09 +0600

    made a basic VPG model that will encode to a mean and standard deviation

--- PPO_model.py
+++ PPO_model.py
@@ -4,6 +4,21 @@ import torch.nn.functional as F
 import torch.optim
 import hyperparameters
 
+# first test with VPG, not PPO
+class VPG(nn.module):
+    def __init__(self, state_space):
+        super().__init__()
+        self.layer_1 = nn.Linear(in_features= state_space, out_features= (state_space -3))
+        self.layer_2 = nn.Linear(in_features= (state_space -3), out_features= (state_space - 8))
+        self.layer_3 = nn.Linear(in_features= (state_space - 8), out_features= 1)
+
+    def forward(self,input):
+        input = F.relu(self.layer_1(input))
+        input = F.relu(self.layer_2(input))
+        return self.layer_3(input)
+
+
+
 class PPO(nn.Module):
     def __init__(self):
         super().__init__()
