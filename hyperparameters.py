c76a127ba56f4cf0ac5a6a62b3ed6fe52a8af1d3
NamgGara <garab.python@gmail.com>
2022-08-10 15:41:39 +0600

    needed action space variable

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,11 +1,12 @@
-
-#hyper parameters
-
 gravity = [0,0,-190.9]
 spawn_point = [0,0,0]
 spawn_pitch = [0,1,0]
+
 urdf_model = "..\\humanoid.urdf"
 plane = "plane.urdf"
+
 simualtion_step = 10000
 simulation_speed = 1./300.
+
 feature_length = 15 + 15
+action_space = 15
