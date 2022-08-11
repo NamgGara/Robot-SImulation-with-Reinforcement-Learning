2e1a8898febba7b0132f9e9437d8c42feb63320c
NamgGara <garab.python@gmail.com>
2022-08-11 18:24:41 +0600

    copied current VPG implementation to folder second_implementation

---
+++ second_implementation/hyperparameters.py
@@ -0,0 +1,17 @@
+gravity = [0,0,-190.9]
+spawn_point = [0,0,0]
+spawn_pitch = [0,1,0]
+
+urdf_model = "..\\humanoid.urdf"
+plane = "plane.urdf"
+
+simualtion_step = 1000
+epoch = 15
+
+simulation_speed = 1./300.
+
+feature_length = 15 + 15
+action_space = 15
+
+VPG_mu_learning_rate = 0.01
+VPG_sigma_learning_rate = 0.01
---
+++ second_implementation/reward_tuning.py
@@ -0,0 +1,30 @@
+
+def overlapping_punishment(list):
+    if not list:
+        return -1
+    return 0
+
+class Reward_for_standing():
+    def __init__(self) -> None:
+        
+        self.threshold = 0
+        self.reward = 5
+        self.standing_reward = 1
+        self.punishment = -1
+        self.gap = 10
+    
+    def set_threshold(self, value):
+        self.threshold = value
+    
+    def reset(self):
+        self.threshold = 0
+
+    def __call__(self,progress):
+        if progress > self.threshold:
+            self.threshold = progress
+            return self.reward
+        elif progress < self.threshold and progress > self.threshold - self.gap:
+            return self.standing_reward
+        return self.punishment
+
+reward = Reward_for_standing()
