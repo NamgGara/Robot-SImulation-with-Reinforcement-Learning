corrected error were gradient of log prob is multiplied with reward (which must happen on after epoch), and in simulation, reward added seems to be cumulative reward which also never gets reset

PPO_model.py
@@ -32,8 +32,6 @@ def save_model():
     torch.save(VPG_mu.state_dict(), "mean_model.pt")
     torch.save(VPG_sigma.state_dict(), "st_dev_model.pt")
 
-
-
 def get_dist_and_action(input, mu=VPG_mu, sigma=VPG_sigma):
     mean = mu(input)
     std = torch.exp(sigma(input))
@@ -44,11 +42,11 @@ def get_dist_and_action(input, mu=VPG_mu, sigma=VPG_sigma):
     
     return dist, dist.sample()
 
-def log_prob_and_tau(action, dist, reward):
-    return -1 * dist.log_prob(action) * reward
+def log_prob_and_tau(action, dist):
+    return -1 * dist.log_prob(action)
       #this is like returning an expectation of the tragetory and reward of tregetory
 
-def training(batch, mu_opt = mu_optimizer, sig_opt = sigma_optimizer):
+def training(batch,reward, mu_opt = mu_optimizer, sig_opt = sigma_optimizer):
     
     mu_opt.zero_grad()
     sig_opt.zero_grad()

simulation.py
@@ -31,7 +31,7 @@ def reset_robot(robot):
     p.removeBody(robot)
     return p.loadURDF(hyperparameters.urdf_model, hyperparameters.spawn_point,
                    p.getQuaternionFromEuler(hyperparameters.spawn_pitch),
-                   flags= p.URDF_USE_SELF_COLLISION)
+                   flags= p.URDF_USE_SELF_COLLISION) , 0
 
 input_tensor = get_states_and_contact()
 
@@ -49,7 +49,7 @@ for a in range(hyperparameters.epoch):
         p.setJointMotorControlArray(robot,joint_array,p.POSITION_CONTROL, action)
 
         c_reward = rt.reward(head_Z_coord()) + rt.overlapping_punishment(p.getContactPoints(robot,robot))
-        batch = torch.cat((batch, PPO_model.log_prob_and_tau(action,dist,c_reward)), 0)
+        batch = torch.cat((batch, PPO_model.log_prob_and_tau(action,dist)), 0)
         input_tensor = get_states_and_contact()
         sleep(hyperparameters.simulation_speed)
 
@@ -58,9 +58,9 @@ for a in range(hyperparameters.epoch):
             
     print(f"rewards are {batch.mean()}")
     print(f"progress was {rt.reward.threshold}")
-    PPO_model.training(batch)
+    PPO_model.training(batch, c_reward)
     batch = torch.tensor([])
-    robot = reset_robot(robot)
+    robot, c_reward = reset_robot(robot)
     PPO_model.save_model()
     rt.reward.reset()
 