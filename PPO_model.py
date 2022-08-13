f49d91150170ace1ff93b083bc7c4b3aed02cd42
NamgGara <garab.python@gmail.com>
2022-08-13 11:05:42 +0600

    added a critic object, changed VPG to nn_model class

--- PPO_model.py
+++ PPO_model.py
@@ -6,7 +6,7 @@ import hyperparameters
 import os
 
 # first test with VPG, not PPO
-class VPG(nn.Module):
+class nn_model(nn.Module):
     def __init__(self, state_space, action_space):
         super().__init__()
         self.layer_1 = nn.Linear(in_features= state_space, out_features= (state_space + 3))
@@ -18,10 +18,11 @@ class VPG(nn.Module):
         input = F.relu(self.layer_2(input))
         return self.layer_3(input)
 
-VPG_mu = VPG(hyperparameters.feature_length, hyperparameters.action_space)
-VPG_sigma = VPG(hyperparameters.feature_length, hyperparameters.action_space)
+VPG_mu = nn_model(hyperparameters.feature_length, hyperparameters.action_space)
+VPG_sigma = nn_model(hyperparameters.feature_length, hyperparameters.action_space)
+Critic = nn_model(hyperparameters.feature_length, 1)
 
-for i,j in zip(["mean_model.pt","st_dev_model.pt"],[VPG_mu, VPG_sigma]):
+for i,j in zip(["mean_model.pt","st_dev_model.pt","critic.pt"],[VPG_mu, VPG_sigma,Critic]):
     if os.path.exists(i):
         j.load_state_dict(torch.load(i))
 
@@ -31,6 +32,7 @@ sigma_optimizer = torch.optim.Adam(VPG_sigma.parameters(),lr=hyperparameters.VPG
 def save_model():
     torch.save(VPG_mu.state_dict(), "mean_model.pt")
     torch.save(VPG_sigma.state_dict(), "st_dev_model.pt")
+    torch.save(Critic.state_dict(), "critic.pt")
 
 def get_dist_and_action(input, mu=VPG_mu, sigma=VPG_sigma):
     mean = mu(input)
@@ -42,7 +44,6 @@ def get_dist_and_action(input, mu=VPG_mu, sigma=VPG_sigma):
 def log_prob_and_tau(action, dist):
     return -1 * dist.log_prob(action)
       #this is like returning an expectation of the tragetory and reward of tregetory
-
 
 def training(batch_of_tregactory,reward, mu_opt = mu_optimizer, sig_opt = sigma_optimizer):
     mu_opt.zero_grad()
