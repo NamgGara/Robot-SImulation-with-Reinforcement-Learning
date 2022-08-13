e001c86f4ada9da41ff140f725983906b4fa92a8
NamgGara <garab.python@gmail.com>
2022-08-13 14:35:24 +0600

    reward tuning refactor

--- reward_tuning.py
+++ reward_tuning.py
@@ -5,7 +5,7 @@ class Reward_for_standing():
         self.reward = 5
         self.standing_reward = 1
         self.punishment = -1
-        self.gap = 2
+        self.gap = 1
     
     def set_threshold(self, value):
         self.threshold = value
@@ -17,7 +17,7 @@ class Reward_for_standing():
         if not list:
             return -1
         return 0
-# put in switch logic for contact that is for overlapping
+
     def __call__(self,progress, contact, idx):
         # if idx%2==0:
         #     return self.overlapping_punishment(contact)
