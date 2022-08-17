88e6e76d0e472094e822d1cf4b724e97d9944e09
NamgGara <garab.python@gmail.com>
2022-08-17 18:30:11 +0600

    added a simple verion of matplotlib

---
+++ graph.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import os
+import seaborn as sns
+
+plt.ion()
+fig, ax= plt.subplots()
+
+class process_plot:
+
+    def __init__(self) -> None:
+        self.value = [0]
+        self.index = 0
+
+    def __call__(self,value):
+        self.value.append(value)
+        self.index +=1
+
+    def graph(self):
+
+        ax.plot(range(self.index),self.value)
+        # plt.draw()
+        plt.savefig("test")
+        # plt.pause(0.1)
+
+graph = process_plot()
+
+graph(10)
+graph(11)
+graph.graph()
