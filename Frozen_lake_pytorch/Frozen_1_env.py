6c07ba45a0f880146b26b59ec409c6958c4c0a71
NamgGara <garab.python@gmail.com>
2022-08-26 19:59:23 +0600

    this model has a perfromance of 64 successes from 100 episodes

--- Frozen_lake_pytorch/Frozen_1_env.py
+++ Frozen_lake_pytorch/Frozen_1_env.py
@@ -1,18 +1,27 @@
 import gym
-import matplotlib.pyplot as plt
 from torch_Frozen_1_model import Model
 
 env = gym.make("FrozenLake-v1")
-old_obs = env.reset()
-model = Model()
+model = Model(env.observation_space.n, env.action_space.n)
+total = []
+total_count = 0
+episodes = 100
+time_step = 100
+for i in range(episodes):
+    old_obs = env.reset()
+    done = False
 
-for i in range(100):
-    env.render()
-    action = model.action(old_obs)
-    new_obs, reward, done, _ = env.step(action)
-    model.Q_value(old_obs, new_obs, action, reward)
-    if done:
-        env.reset()
+    for i in range(time_step):
+        # env.render()
+        action = model.action(old_obs)
+        new_obs, reward, done, _ = env.step(action)
+        model.Q_value(old_obs, new_obs, action, reward)
+        old_obs = new_obs
+        if done:
+            total.append(reward)
+            total_count+=reward
+            break
 
-print(model.q_table)
+model.reward_plot(total)
+print(total_count)
 
--- Frozen_lake_pytorch/torch_Frozen_1_model.py
+++ Frozen_lake_pytorch/torch_Frozen_1_model.py
@@ -1,20 +1,44 @@
 import torch
 
 class Model:
-    def __init__(self, env_obs_space=16, env_action_space=4) -> None:
-        self.q_table = torch.zeros(size=(env_obs_space, env_action_space))
+    def __init__(self, env_obs_space, env_action_space) -> None:
         self.gamma = 0.9
         self.epsilon = 1
-        self.epsilon_decay = 0.01
+        self.epsilon_decay = 0.99
+        self.lr = 0.1
+        self.q_table = torch.zeros(size=(env_obs_space, env_action_space))
+        
+        #testing
+        self.epsilon = 0
+        self.q_table = torch.tensor([[0.0613, 0.0605, 0.0605, 0.0587],
+        [0.0412, 0.0452, 0.0447, 0.0576],
+        [0.0745, 0.0679, 0.0679, 0.0581],
+        [0.0388, 0.0394, 0.0324, 0.0539],
+        [0.0822, 0.0692, 0.0608, 0.0492],
+        [0.0000, 0.0000, 0.0000, 0.0000],
+        [0.1090, 0.0924, 0.0999, 0.0212],
+        [0.0000, 0.0000, 0.0000, 0.0000],
+        [0.0716, 0.1020, 0.0983, 0.1321],
+        [0.1368, 0.2359, 0.2254, 0.1298],
+        [0.2827, 0.2606, 0.2153, 0.0967],
+        [0.0000, 0.0000, 0.0000, 0.0000],
+        [0.0000, 0.0000, 0.0000, 0.0000],
+        [0.1711, 0.2982, 0.3691, 0.2378],
+        [0.3918, 0.6213, 0.5848, 0.5838],
+        [0.0000, 0.0000, 0.0000, 0.0000]])
 
     def Q_value(self,old_obs, new_obs, action, reward):
-        Q = self.q_table
-        Q[old_obs,action] = Q[old_obs,action] - (reward + self.gamma*torch.max(Q[new_obs]) - Q[old_obs,action])
-        print(Q[0])
+        self.q_table[old_obs,action] = self.q_table[old_obs,action] + self.lr*(reward + self.gamma*torch.max(self.q_table[new_obs]) - self.q_table[old_obs,action])
 
     def action(self,state):
-        self.epsilon = max(0.1, self.epsilon - self.epsilon_decay)
-        if torch.rand(size=(1,)) < self.epsilon:
-            return torch.randint(0, 4, size=(1,)).item()
-        return int(torch.max(self.q_table[state]).item())
+        # self.epsilon = max(0.1, self.epsilon*self.epsilon_decay)
+        # if torch.rand(size=(1,)) < self.epsilon:
+        #     return torch.randint(0, 4, size=(1,)).item()
+        return int(torch.argmax(self.q_table[state]).item())
 
+    @staticmethod
+    def reward_plot(reward):
+        import matplotlib.pyplot as plt
+        plt.figure(figsize=(18,9))
+        plt.plot(reward)
+        plt.show()
